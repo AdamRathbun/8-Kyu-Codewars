b339783bb20b37bebf7e7f97881e5296645482a1
AdamRathbun <hesle1119@gmail.com>
2022-05-18 20:04:46 -0400

    t27

---
+++ Return Negative.js
@@ -0,0 +1,10 @@
+function makeNegative(num) {
+    return num===Math.abs(num) ? -num : num
+  }
+  
+  /*
+  P: num
+  R: negative num
+  E: function(-5) returns -5 function(5) returns -5
+  P: conditional
+  */
